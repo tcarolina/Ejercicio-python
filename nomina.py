8d8a9f2205acf175ec05be7332f1e579a9050159
tcarolina <tmurillo7@misena.edu.co>
2022-07-30 07:32:01 -0500

    se agrega la carpeta

---
+++ nomina.py
@@ -0,0 +1,15 @@
+import time
+import contextlib
+import re
+
+
+def nomina():
+    # Declaracion de las variables
+    su_name = input("Digite Su Nombre: ")
+    su_last = input("Digite Su Apellido: ")
+    su_Documento = input("Ingrese su numero de Documento")
+     sueldo_bruto = int(input("Digite Su Salario: "))
+    su_Diastrabajados = int(input("Digite dia trabajados: "))
+    time.sleep(10)
+    print("\n")
+    
